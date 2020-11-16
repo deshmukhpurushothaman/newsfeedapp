96dd56674e22849dc66942cac525750490b7ce2d
Deshmukh P <deshmukhpurushothaman@gmail.com>
2020-11-16 14:56:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'Start.dart';
+import 'package:flutter/cupertino.dart';
+import 'admin_home.dart';
+import 'Authentication/auth_helper.dart';
+import 'home.dart';
+import 'Authentication/signup.dart';
+import 'package:flutter/material.dart';
+import 'Authentication/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Newsfeed app',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            UserHelper.saveUser(snapshot.data);
+            return StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("users")
+                  .doc(snapshot.data.uid)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (snapshot.hasData && snapshot.data != null) {
+                  final userDoc = snapshot.data;
+                  final user = userDoc.data();
+                  if (user['role'] == 'admin') {
+                    return AdminHome();
+                  } else {
+                    return Home();
+                  }
+                } else {
+                  return Material(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+              },
+            );
+          }
+          return Start();
+        });
+  }
+}
